8d70e9b6344cf43524c3a78e1ec63df4bd6d934a
Jaksa <jmilanovic@truveris.com>
2023-03-21 17:17:26 +0100

    Fix lovely-pytest-docker

--- tests/fixtures/docker.py
+++ tests/fixtures/docker.py
@@ -1,9 +1,11 @@
+import time
+
 from lovely.pytest.docker.compose import Services
 from pytest_asyncio import fixture
 
 
-@fixture(scope="session", autouse=False)
+@fixture(scope="session", autouse=True)
 async def docker_juice_shop(docker_services: Services) -> None:
     test_db_service_name: str = "juice-shop"
     docker_services.start(test_db_service_name)
-    docker_services.wait_for_service(test_db_service_name, 3000, timeout=60)
+    time.sleep(2)
