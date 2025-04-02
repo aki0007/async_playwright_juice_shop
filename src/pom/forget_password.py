6e0d6edd2d15c1c56ea465deed2911c7a8b492f2
aki007 <jaksa.milanovic007@gmail.com>
2025-04-02 12:31:53 +0300

    Ignore mypy

--- src/pom/forget_password.py
+++ src/pom/forget_password.py
@@ -8,6 +8,7 @@ class ForgetPasswordPage:
     NEW_PASSWORD: str = "#newPassword"
     NEW_PASSWORD_REPEAT: str = "#newPasswordRepeat"
     SECURITY_ANSWER: str = "#securityAnswer"
+    CHANGE_BUTTON: str = "span:has-text('Change')"
 
     def __init__(self, page: Page) -> None:
         self.page: Page = page
@@ -19,3 +20,4 @@ class ForgetPasswordPage:
         await self.page.locator(self.SECURITY_ANSWER).fill(security_answer)
         await self.page.locator(self.NEW_PASSWORD).fill(password)
         await self.page.locator(self.NEW_PASSWORD_REPEAT).fill(password)
+        await self.page.locator(self.CHANGE_BUTTON).click()
